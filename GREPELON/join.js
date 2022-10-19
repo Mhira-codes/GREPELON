c7f4e21e2f4835a3e9888cd2c938636a5f443f72
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-19 16:26:30 +0000

    file updated

---
+++ GREPELON/join.js
@@ -0,0 +1,8 @@
+let popUp = document.getElementById('popup')
+function whenClicked() {
+    popUp.classList.add('popup-appear')
+   
+}
+function Close() {
+    popUp.classList.remove('popup-appear')
+}
